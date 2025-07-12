a2a4c75239391bb584629754942974797a17c9ca
bel-n <nade.belovinova@gmail.com>
2025-07-12 16:23:38 +0200

    Added a CachedThreadPool for handling the review stream.

--- src/main/java/parallel/ParallelReviewDS.java
+++ src/main/java/parallel/ParallelReviewDS.java
@@ -1,4 +1,30 @@
 package parallel;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import sequential.Pipeline; //because nothing changes
+
 public class ParallelReviewDS {
+private final ExecutorService executorService;
+
+private final Pipeline pipeline;
+
+public ParallelReviewDS(){
+    this.executorService = Executors.newCachedThreadPool();
+    this.pipeline = new Pipeline();
+}
+
+public void processReview (String review){
+    executorService.submit(() -> {
+        String sentiment = pipeline.analyzeSentiment(review);
+        System.out.println("Sentiment:" + sentiment);
+    });
+}
+
+public void shutdown(){
+    executorService.shutdown();
+}
+
 }
