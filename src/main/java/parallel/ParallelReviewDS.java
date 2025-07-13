4df0a32e3d0d96dda4f7973bf1bda4adcd568f62
bel-n <nade.belovinova@gmail.com>
2025-07-13 20:15:59 +0200

    Removed unnecessary code

--- src/main/java/parallel/ParallelReviewDS.java
+++ src/main/java/parallel/ParallelReviewDS.java
@@ -1,30 +1,31 @@
 package parallel;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executor; //executor is interface for executing tasks
+import java.util.concurrent.ExecutorService; //manages a pool of threads
 import java.util.concurrent.Executors;
 
 import sequential.Pipeline; //because nothing changes
 
 public class ParallelReviewDS {
-private final ExecutorService executorService;
+private final ExecutorService executorService; //for pool of threads
 
 private final Pipeline pipeline;
 
 public ParallelReviewDS(){
     this.executorService = Executors.newCachedThreadPool();
+    //a thread pool that can dynamically grow as needed.
+    //it will reuse idle threads when possible.
     this.pipeline = new Pipeline();
+    //handles sentiment analysis
 }
 
 public void processReview (String review){
-    executorService.submit(() -> {
+    executorService.submit(() -> { //submits a task to be run in a separate thread
+        //every thread performs the following  :
         String sentiment = pipeline.analyzeSentiment(review);
         System.out.println("Sentiment:" + sentiment);
     });
 }
 
-public void shutdown(){
-    executorService.shutdown();
-}
 
 }
