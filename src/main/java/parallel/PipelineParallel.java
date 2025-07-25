9c5130aaee9b7c3b7885e9d26b49f16218f1868d
bel-n <nade@jafcglobal.com>
2025-07-25 15:37:33 +0200

    feat : create parallel pipeline

---
+++ src/main/java/parallel/PipelineParallel.java
@@ -0,0 +1,38 @@
+package parallel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreSentence;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import java.util.*;
+
+
+
+public class PipelineParallel {
+    private final StanfordCoreNLP pipeline;
+
+    public PipelineParallel() {
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos,lemma,ner,parse,sentiment");
+        props.setProperty("threadsafe", "true");
+
+        this.pipeline = new StanfordCoreNLP(props);
+    }
+
+
+    public String analyzeSentiment(String review) {
+        try{
+            Annotation annotation = new Annotation(review);
+            this.pipeline.annotate(annotation);
+            CoreDocument coreDocument  = new CoreDocument(annotation);
+
+            if(!coreDocument.sentences().isEmpty()){
+                CoreSentence sentence = coreDocument.sentences().get(0);
+                return sentence.sentiment();
+            }
+            return "Unknown";
+        }catch (Exception e){
+            System.err.println("Error analyzing sentiment: " + e.getMessage());
+            return "Error";
+        }
+    }
+}
