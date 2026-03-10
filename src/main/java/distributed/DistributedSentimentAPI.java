25736e2e6d5068abf4cfb1f1fa42e90df6995820
bel-n <nade.belovinova@gmail.com>
2026-03-10 16:19:30 +0100

    init of MPI and id. process rank

---
+++ src/main/java/distributed/DistributedSentimentAPI.java
@@ -0,0 +1,23 @@
+package distributed;
+
+import mpi.*;
+import util.LogLevel;
+import util.Logger;
+
+public class DistributedSentimentAPI {
+     public static void main(String[] args){
+         MPI.Init(args);
+
+         int rank = MPI.COMM_WORLD.Rank();
+         int size = MPI.COMM_WORLD.Size();
+
+         //dispatcher + worker logic
+
+         Logger.log("Process" + rank + " of size: " + size +  "started", LogLevel.Update);
+
+         MPI.Finalize();
+
+
+
+     }
+}
