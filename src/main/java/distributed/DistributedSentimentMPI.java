e8d38ecebfb8658f5d3386b7beaac7def5caaaff
bel-n <nade.belovinova@gmail.com>
2026-03-10 21:07:37 +0100

    implementation plan for the distributed methods

--- src/main/java/distributed/DistributedSentimentMPI.java
+++ src/main/java/distributed/DistributedSentimentMPI.java
@@ -19,12 +19,20 @@ public class DistributedSentimentMPI {
 
      }
 
-     private static void startDispatcher(){}
+     private static void startDispatcher(){
+         //should open connection to the server and subscribe
+         //review comes and like in the other parts gets put in a queue
+         //here the dispatcher will get the reviews and distribute them among processes
+     }
 
-    private static void startWorkersMPI(){}
+    private static void startWorkersMPI(){
+         //every process should have its own pipeline
+        //the worker sends back its work to dispatcher and goes back to waiting for new review
+    }
 
-    private static void DispatcherWebSocketClient(){}
-
+    private static void DispatcherWebSocketClient(){
+         //will manage the connection to the server and subscribe to the same topics as in the other parts
+    }
 
 
 }
